69fd42b2132a4ca231e16bda16694353b387a580
LadySith <me.sithe.ncube@gmail.com>
2021-01-17 01:44:15 +0200

    Eye-tracking works!!!!
    Eye tracking movement works. Also added low poly plants package.

--- Assets/Scripts/FollowMouse.cs
+++ Assets/Scripts/FollowMouse.cs
@@ -32,6 +32,7 @@ public class FollowMouse : MonoBehaviour
 
         if (valueNUI == 1)
         {
+
             if (Input.GetMouseButtonDown(0))
             {
                 if (Physics.Raycast(castPoint, out hit, Mathf.Infinity, interactiveLayer) && (Vector3.Distance(new Vector3(hit.point.x, hit.point.y, hit.point.z), transform.position)) > stopping_distance)
@@ -48,9 +49,18 @@ public class FollowMouse : MonoBehaviour
             {
                 Debug.Log("Tobii eyetracker detected");
                 GazePoint gazePoint = TobiiAPI.GetGazePoint();
+
+                Ray castPointTobii = Camera.main.ScreenPointToRay(new Vector3(gazePoint.Screen.x,gazePoint.Screen.y,0));
+                RaycastHit hitTobii;
                 if (gazePoint.IsRecent())
                 {
                     //Here we use the eyetracking point to move the cursor
+                    if (Physics.Raycast(castPointTobii, out hitTobii, Mathf.Infinity, interactiveLayer) && (Vector3.Distance(new Vector3(hitTobii.point.x, hitTobii.point.y, hitTobii.point.z), transform.position)) > stopping_distance)
+                    {
+                        targetPosition = new Vector3(hitTobii.point.x, hitTobii.point.y, hitTobii.point.z);
+                        transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+                    }
+                    transform.LookAt(targetPosition);
 
                     // Note: Values can be negative if the user looks outside the game view. 
                     // print("Gaze point on Screen (X,Y): " + gazePoint.Screen.x + ", " + gazePoint.Screen.y);
