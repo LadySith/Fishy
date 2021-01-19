e57109e8a8caedf061e29928ca289c46e4b5c753
LadySith <me.sithe.ncube@gmail.com>
2021-01-19 06:01:55 +0200

    Added Music
    Added Music

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+
+    private static MusicManager MusicManagerInstance;
+
+    void Awake()
+    {
+        DontDestroyOnLoad(this);
+
+        if (MusicManagerInstance == null)
+        {
+            MusicManagerInstance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
